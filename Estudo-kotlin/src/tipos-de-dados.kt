ad09976fb5f44e2f125868f4220ac333dd9063b6
Luxmartins <lucasnmartins@outlook.com.br>
2021-03-06 13:45:05 -0300

    tipos de dados

---
+++ Estudo-kotlin/src/tipos-de-dados.kt
@@ -0,0 +1,19 @@
+// Tipos de dados no seu valor máximo
+fun main() {
+    println(Int.MAX_VALUE)
+    println(Float.MAX_VALUE)
+    println(Long.MAX_VALUE)
+    println(Byte.MAX_VALUE)
+    println(Short.MAX_VALUE)
+
+}
+
+/* conversão de dados
+    toByte()
+    toShot()
+    toInt()
+    toLong()
+    toFloat()
+    toDouble()
+    toChar()
+ */
