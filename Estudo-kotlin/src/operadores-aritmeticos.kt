4ad276fae28fdc37b5015b4353929f6b07070bb0
Luxmartins <lucasnmartins@outlook.com.br>
2021-03-06 21:04:49 -0300

    Operadores matemáticos

---
+++ Estudo-kotlin/src/operadores-aritmeticos.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var a = 10
+    var b = 5
+    // a soma pode ser escrito dessa forma com expressões matemáticass
+    println(a + b) // Soma
+    println(a - b) // Subtração
+    println(a * b) // Multiplicação
+    println(a / b) // Divisão
+    println(a % b) // Resto
+
+    // Ou dessa forma usando comandos
+    println(a.plus(b)) // Soma
+    println(a.minus(b)) // Subtração
+    println(a.times(b)) // Multiplicação
+    println(a.div(b)) // Divisão
+
+
+    val count = 10
+    val times = 10
+    val r = times + count
+    println(r)
+    print(count.plus(times))
+
+}
